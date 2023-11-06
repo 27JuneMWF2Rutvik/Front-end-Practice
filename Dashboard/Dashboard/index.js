8efe446e1867cabf090f56f46018eb5d853f2d38
Rutvikbhambhi <bhambhirutvik2004@gmail.com>
2023-11-06 15:41:05 +0530

    Dashboard

--- Dashboard/Dashboard/index.js
+++ Dashboard/Dashboard/index.js
@@ -1,3 +1,36 @@
+// const form = document.querySelector('form')
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault()
+
+//     const username = form.username.value;
+//     const password = form.password.value;
+
+//     const authenticated = authentication(username, password)
+
+//     if(authenticated){
+//         window.location.href = "Dashboard.html"
+//     } else {
+//         alert("wrong")
+//     }
+// })
+
+// // function for checking username and password 
+// function authentication(username, password){
+//     if(username === "admin" && password === "password") {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+
+
+
+
+
+
+
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
